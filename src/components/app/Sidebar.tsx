1921965ad9d05e0c4ffb135596f0271db1e6c7e1
Hendrik Schuster <h.schuster@deviantdev.com>
2021-03-17 23:40:46 +0100

    Replace the Timeline component with a simple list

--- src/components/app/Sidebar.tsx
+++ src/components/app/Sidebar.tsx
@@ -1,14 +1,29 @@
-import { Box, createStyles, IconButton, makeStyles, Paper, TextField, Theme, Typography } from '@material-ui/core';
+import {
+  Avatar,
+  Box,
+  createStyles,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+  ListItemText,
+  makeStyles,
+  TextField,
+  Theme,
+  Tooltip,
+  Typography
+} from '@material-ui/core';
 import CheckIcon from '@material-ui/icons/Check';
+import DeleteIcon from '@material-ui/icons/Delete';
 import ErrorIcon from '@material-ui/icons/Error';
 import FlagIcon from '@material-ui/icons/Flag';
 import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
-import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineOppositeContent, TimelineSeparator } from '@material-ui/lab';
 import React, { useContext, useRef } from 'react';
 import { useMutation, useQuery } from 'react-query';
 import { AppContext } from '../../pages/app';
-import { CreateIssueMutation, SetActiveIssuesMutation, IssueQuery } from '../../utils/mongodb/mongodb.actions';
+import { CreateIssueMutation, IssueQuery, SetActiveIssuesMutation } from '../../utils/mongodb/mongodb.actions';
 import { IssueState } from '../../utils/types';
 
 const Estimate = () => {
@@ -32,6 +47,7 @@ const Estimate = () => {
   };
 
   const handleSetActiveIssue = (issueId: string) => {
+    console.log(issueId);
     activeIssueMutation.mutate({
       issueId
     });
@@ -64,37 +80,46 @@ const Estimate = () => {
         />
       </Box>
       <Box flexGrow={1} flexBasis={0} overflow={'auto'}>
-        <Timeline align="left" className={classes.timeline}>
-          {issueQuery.data?.map(issue => (
-            <TimelineItem key={issue._id}>
-              <TimelineOppositeContent className={classes.oppositeContent} />
-              <TimelineSeparator>
-                <TimelineDot color="primary" variant="outlined" className={classes.issueDot}>
-                  {issue.state === IssueState.OPEN && <RadioButtonUncheckedIcon />}
-                  {issue.state === IssueState.ESTIMATED && <CheckIcon />}
-                  {issue.state === IssueState.UNFINISHED && <ErrorIcon />}
-                </TimelineDot>
-                <TimelineConnector className={classes.secondaryTail} />
-              </TimelineSeparator>
-              <TimelineContent>
-                <Paper elevation={0} className={classes.timelineItemContent} onClick={e => handleSetActiveIssue(issue._id)}>
-                  {issue.name}
-                  {issue.estimate && <br />}
-                  {issue.estimate && `Estimated Value was ${issue.estimate}`}
-                </Paper>
-              </TimelineContent>
-            </TimelineItem>
+        <List component="nav">
+          {issueQuery.data?.map((issue, issueIndex) => (
+            <>
+              <ListItem key={issueIndex} button disableGutters>
+                <ListItemAvatar>
+                  <Tooltip title={issue.state}>
+                    <Avatar className={classes.issueDot}>
+                      <>
+                        {issue.state === IssueState.OPEN && <RadioButtonUncheckedIcon />}
+                        {issue.state === IssueState.ESTIMATED && <CheckIcon />}
+                        {issue.state === IssueState.UNFINISHED && <ErrorIcon />}
+                      </>
+                    </Avatar>
+                  </Tooltip>
+                </ListItemAvatar>
+                <ListItemText primary={issue.name} secondary={issue._id} />
+                <ListItemSecondaryAction>
+                  <IconButton edge="end" aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+              <div className={classes.vertical}></div>
+            </>
           ))}
-          <TimelineItem>
-            <TimelineOppositeContent className={classes.oppositeContent} />
-            <TimelineSeparator>
-              <TimelineDot>
+
+          <ListItem disableGutters>
+            <ListItemAvatar>
+              <Avatar>
                 <FlagIcon />
-              </TimelineDot>
-            </TimelineSeparator>
-            <TimelineContent>This is the beginning of your estimation history! Create a new story to start estimating.</TimelineContent>
-          </TimelineItem>
-        </Timeline>
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="This is the beginning of your estimation history! Create a new story to start estimating." />
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="delete">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </List>
       </Box>
     </Box>
   );
@@ -104,7 +129,19 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       height: '100%',
-      paddingLeft: theme.spacing(1)
+      paddingRight: theme.spacing(2),
+      paddingLeft: theme.spacing(2)
+    },
+    vertical: {
+      borderLeft: '1px solid lightgrey',
+      height: theme.spacing(4),
+      marginLeft: theme.spacing(2.5),
+      marginTop: '-' + theme.spacing(2) + 'px',
+      marginBottom: '-' + theme.spacing(2) + 'px'
+    },
+    small: {
+      width: theme.spacing(3),
+      height: theme.spacing(3)
     },
     header: {
       paddingTop: theme.spacing(4),
@@ -114,26 +151,9 @@ const useStyles = makeStyles((theme: Theme) =>
       paddingRight: theme.spacing(1),
       paddingBottom: theme.spacing(2)
     },
-    timeline: {
-      paddingLeft: 0,
-      paddingRight: 0
-    },
-    oppositeContent: {
-      flex: 0,
-      padding: 0
-    },
-    secondaryTail: {
-      backgroundColor: theme.palette.secondary.main
-    },
-    timelineItemContent: {
-      padding: theme.spacing(1),
-      cursor: 'pointer',
-      '&:hover': {
-        background: 'rgba(0,0,0,0.1)'
-      }
-    },
     issueDot: {
-      borderColor: 'transparent'
+      background: 'transparent',
+      color: 'grey'
     },
     createIssue: {
       '& .MuiOutlinedInput-adornedEnd': {
