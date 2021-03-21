f9779d53ba5fe2b677767e8dd69e1e31be3fb3eb
Hendrik Schuster <h.schuster@deviantdev.com>
2021-03-21 17:35:32 +0100

    Wip  Tabs

--- src/components/app/estimate/EstimationPanel.tsx
+++ src/components/app/estimate/EstimationPanel.tsx
@@ -1,4 +1,5 @@
-import { createStyles, Grid, makeStyles, Paper, Theme } from '@material-ui/core';
+import { AppBar, createStyles, Grid, makeStyles, Paper, Tab, Theme } from '@material-ui/core';
+import { TabContext, TabList, TabPanel } from '@material-ui/lab';
 import React from 'react';
 import { useGetEstimatedIssueQuery } from '../../../apollo/types.grapqhl';
 import EstimationPanelCard from './EstimationPanelCard';
@@ -8,17 +9,36 @@ const estimationValues = ['1', '2', '3', '5', '8', '13', '20', '40', '100'];
 export default function EstimationPanel() {
   const classes = useStyles();
 
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+    setValue(newValue);
+  };
+
   const { data } = useGetEstimatedIssueQuery();
 
   return (
     <Paper elevation={0} className={classes.cards}>
-      <Grid direction="row" justify="center" alignItems="stretch" container>
-        {estimationValues.map((value, index) => (
-          <Grid item xs={3} md={2} className={classes.cardsContent} key={index}>
-            <EstimationPanelCard value={value} issue={data?.activeTeam?.estimatedIssue} />
+      <TabContext value={value}>
+        <AppBar position="static">
+          <TabList onChange={handleChange} aria-label="simple tabs example">
+            <Tab label="Item One" value="1" />
+            <Tab label="Item Two" value="2" />
+            <Tab label="Item Three" value="3" />
+          </TabList>
+        </AppBar>
+        <TabPanel value="1">
+          <Grid direction="row" justify="center" alignItems="stretch" container>
+            {estimationValues.map((value, index) => (
+              <Grid item xs={3} md={2} className={classes.cardsContent} key={index}>
+                <EstimationPanelCard value={value} issue={data?.activeTeam?.estimatedIssue} />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+        </TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+      </TabContext>
     </Paper>
   );
 }
@@ -26,6 +46,7 @@ export default function EstimationPanel() {
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     cards: {
+      flexGrow: 1,
       backgroundColor: theme.palette.grey[100],
       marginBottom: theme.spacing(2)
     },
