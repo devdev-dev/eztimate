0a8d82b187ade0fba2c99ad3264d0d1fd5a28b1a
Hendrik Schuster <h.schuster@deviantdev.com>
2021-04-20 00:33:38 +0200

    Rename environment variables to reflect S3 relevance

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -21,15 +21,15 @@ export default async function handler(req, res) {
   });
 
   aws.config.update({
-    accessKeyId: process.env.ACCESS_KEY,
-    secretAccessKey: process.env.SECRET_KEY,
-    region: process.env.REGION,
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SECRET_KEY,
+    region: process.env.S3_REGION,
     signatureVersion: 'v4'
   });
 
   await new aws.S3().upload(
     {
-      Bucket: process.env.BUCKET_NAME,
+      Bucket: process.env.S3_BUCKET_NAME,
       Key: reqForm.name + '.jpeg',
       Body: reqForm.data,
       ACL: 'public-read'
