5a204e9beaa3ec8461c21262869d9d1548f84011
Hendrik Schuster <h.schuster@deviantdev.com>
2021-04-25 09:36:41 +0200

    Add a gql query to access the currently active issue for the active team

--- src/apollo/resolvers/queryResolvers.ts
+++ src/apollo/resolvers/queryResolvers.ts
@@ -11,6 +11,28 @@ export const resolvers: IResolvers = {
     activeTeam: async (parent, args, { db, context: { req, res } }) => {
       const team = await db.collection('teams').findOne({ _id: getObjectId(new Cookies(req, res).get(CookieName.TEAM_ID)) });
       return team;
+    },
+    activeIssue: async (parent, args, { db, context: { req, res } }) => {
+      const activeIssue = await db
+        .collection('teams')
+        .aggregate([
+          { $match: { _id: getObjectId(new Cookies(req, res).get(CookieName.TEAM_ID)) } },
+          {
+            $lookup: {
+              from: 'issues',
+              localField: 'estimatedIssue',
+              foreignField: '_id',
+              as: 'estimatedIssue'
+            }
+          },
+          { $unwind: '$estimatedIssue' },
+          { $replaceRoot: { newRoot: '$estimatedIssue' } }
+        ])
+        .toArray();
+
+      console.log(activeIssue);
+
+      return activeIssue[0];
     }
   }
 };
