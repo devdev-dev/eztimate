0cc3bc479ae17044b067f4ea67d399bbfd69ae2b
Hendrik Schuster <h.schuster@deviantdev.com>
2021-07-07 22:09:33 +0200

    Restructure pages folder below src

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import styles from '../../styles/Home.module.css';
 import {UserProvider} from "@auth0/nextjs-auth0";
 
 export default function Home() {
