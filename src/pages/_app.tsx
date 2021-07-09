3854ac50a58ed514c3f49b095c41b490f797516f
Hendrik Schuster <h.schuster@deviantdev.com>
2021-07-09 22:54:11 +0200

    WIP Formatting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
 import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Eztimate | Easy Estimation</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </>
+  );
 }
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,43 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/Home.module.css';
-import {UserProvider} from "@auth0/nextjs-auth0";
+
+import {useUser} from "@auth0/nextjs-auth0";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Eztimate - Easy estimations</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const {user, error, isLoading} = useUser();
+    return (
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Eztimate!</a>
-        </h1>
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Welcome to <a href="https://nextjs.org">Eztimate!</a>
+                </h1>
 
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
-      </main>
+                <p className={styles.description}>
+                    {isLoading && <div>Loading...</div>}
+                    {error && <div>{error.message}</div>}
+                    {user && (<div>
+                        <h2>{user.name}</h2>
+                        <p>{user.email}</p>
+                        <a href="/api/auth/logout">Logout</a>
+                    </div>)}
+                    {!isLoading && !error && !user && <a href="/api/auth/login">Login</a>}
+                </p>
+            </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <footer className={styles.footer}>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{' '}
+                    <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16}/>
           </span>
-        </a>
-      </footer>
-    </div>
-  );
+                </a>
+            </footer>
+        </div>
+    );
 }
