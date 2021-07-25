cb4af1c9749aca5e0edae3ec5a02aed89341bb66
Hendrik Schuster <h.schuster@deviantdev.com>
2021-07-25 09:40:21 +0200

    Still trying things

---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import theme from '../src/theme';
+import { UserProvider } from '@auth0/nextjs-auth0';
+
+const cache = createCache({ key: 'css', prepend: true });
+cache.compat = true;
+
+export default function MyApp(props: AppProps) {
+  const { Component, pageProps } = props;
+  return (
+    <CacheProvider value={cache}>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <UserProvider>
+          <Component {...pageProps} />
+        </UserProvider>
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
--- src/pages/team/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Team() {
-  return <div>Team</div>;
-}
