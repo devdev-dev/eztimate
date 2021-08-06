ce382b705b82493cbf75b0b8456bd9f70d718901
Hendrik Schuster <h.schuster@deviantdev.com>
2021-08-06 22:19:34 +0200

    Extend Estimation Toolbar with a hide/reveal button

--- src/components/estimate/EstimationToolbar.tsx
+++ src/components/estimate/EstimationToolbar.tsx
@@ -1,23 +1,28 @@
-import * as React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import Box from '@material-ui/core/Box';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import ReplayIcon from '@material-ui/icons/Replay';
+import * as React from 'react';
+import { useState } from 'react';
 
 export default function ButtonAppBar() {
+  const [isRevealed, setRevealed] = useState(false);
+
   return (
     <AppBar position="static" color="transparent" elevation={0}>
       <Toolbar>
+        <IconButton size="large" aria-label="search" color="inherit" title="New Estimation">
+          <ReplayIcon />
+        </IconButton>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Estimate
         </Typography>
-        <IconButton size="large" aria-label="search" color="inherit">
-          <ReplayIcon />
-        </IconButton>
+
+        <Button variant="contained" color="primary" onClick={() => setRevealed(!isRevealed)}>
+          {isRevealed ? 'Hide Results' : 'Reveal Results'}
+        </Button>
       </Toolbar>
     </AppBar>
   );
