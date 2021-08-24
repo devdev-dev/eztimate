45579217c95493fab88327d8b82c86b015389678
Hendrik Schuster <h.schuster@deviantdev.com>
2021-08-24 20:24:59 +0200

    Add a first query to query the active estimation object

--- src/server/graphql/resolvers/basicResolvers.ts
+++ src/server/graphql/resolvers/basicResolvers.ts
@@ -1,3 +1,12 @@
 import { IResolvers } from '@graphql-tools/utils';
+import Cookies from 'cookies';
+import { CookieName } from '../../../cookies';
 
-export const resolvers: IResolvers = {};
+export const resolvers: IResolvers = {
+  Query: {
+    async activeEstimate(parent, args, { req, res }) {
+      const estimateId = new Cookies(req, res).get(CookieName.ESTIMATE_ID);
+      return { _id: estimateId };
+    }
+  }
+};
