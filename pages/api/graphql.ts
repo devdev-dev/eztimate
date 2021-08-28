4d16eae7d46f9e7ba5d0cd6735d991bec8b4dc04
Hendrik Schuster <h.schuster@deviantdev.com>
2021-08-28 10:45:51 +0200

    Get Apollo Studio running with the local development environment

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,27 +1,27 @@
-import { ApolloServerPluginLandingPageDisabled, ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import { ApolloServer } from 'apollo-server-micro';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { basicResolvers, mutationResolvers, queryResolvers } from '../../src/server/graphql/resolvers';
 import * as typeDefs from '../../src/server/graphql/schema.graphql';
 
 console.time('Apollo Server Startup Time');
+
 const apolloServer = new ApolloServer({
   typeDefs,
-  resolvers: { ...basicResolvers, ...queryResolvers, ...mutationResolvers },
-  plugins: [
-    process.env.NODE_ENV === 'production'
-      ? ApolloServerPluginLandingPageDisabled()
-      : ApolloServerPluginLandingPageGraphQLPlayground({
-          settings: {
-            'request.credentials': 'include'
-          }
-        })
-  ]
+  resolvers: { ...basicResolvers, ...queryResolvers, ...mutationResolvers }
 });
 
 const startServer = apolloServer.start();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', 'https://studio.apollographql.com');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  if (req.method === 'OPTIONS') {
+    res.end();
+    return false;
+  }
+
   await startServer;
   await apolloServer.createHandler({
     path: '/api/graphql'
--- src/server/graphql/resolvers/basicResolvers.ts
+++ src/server/graphql/resolvers/basicResolvers.ts
@@ -4,13 +4,11 @@ import { Estimate, EstimateValue, User } from '../../../generated/graphql';
 export const resolvers: IResolvers = {
   Estimate: {
     values(estimate: Estimate, args, { db }): EstimateValue[] {
-      console.log('resolving Estimate');
       return [];
     }
   },
   EstimateValue: {
     user(estimate: EstimateValue, args, { db }): User {
-      console.log('resolving EstimateValue');
       return { _id: '123' };
     }
   }
