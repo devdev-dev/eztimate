b53a6edbeef3b6bd323c4f531f3a9f8ef60f5236
Hendrik Schuster <h.schuster@deviantdev.com>
2021-08-28 08:37:33 +0200

    Implement a basic graphql query  to access a mock estimate data set

--- src/server/graphql/resolvers/basicResolvers.ts
+++ src/server/graphql/resolvers/basicResolvers.ts
@@ -1,12 +1,15 @@
 import { IResolvers } from '@graphql-tools/utils';
-import Cookies from 'cookies';
-import { CookieName } from '../../../cookies';
+import { Estimate, EstimateValue, User } from '../../../generated/graphql';
 
 export const resolvers: IResolvers = {
-  Query: {
-    async activeEstimate(parent, args, { req, res }) {
-      const estimateId = new Cookies(req, res).get(CookieName.ESTIMATE_ID);
-      return { _id: estimateId };
+  Estimate: {
+    async values(estimate: Estimate, args, { db }): Promise<EstimateValue[]> {
+      return [];
+    }
+  },
+  EstimateValue: {
+    async user(estimate: EstimateValue, args, { db }): Promise<User> {
+      return { _id: '123' };
     }
   }
 };
--- src/server/graphql/resolvers/queryResolvers.ts
+++ src/server/graphql/resolvers/queryResolvers.ts
@@ -1,3 +1,10 @@
 import { IResolvers } from '@graphql-tools/utils';
+import { Estimate, EstimateState } from '../../../generated/graphql';
 
-export const resolvers: IResolvers = {};
+export const resolvers: IResolvers = {
+  Query: {
+    async getActiveEstimate(parent, args, context): Promise<Estimate> {
+      return { _id: '', state: EstimateState.Collect, values: [] };
+    }
+  }
+};
