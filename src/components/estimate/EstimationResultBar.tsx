24bf9ae99769c1ddc4bdc20e7a209d5dba3f2e71
Hendrik Schuster <h.schuster@deviantdev.com>
2021-09-06 23:13:10 +0200

    Rework toolbar styles to rename issue and add AvatarGroups

--- src/components/estimate/EstimationResultBar.tsx
+++ src/components/estimate/EstimationResultBar.tsx
@@ -1,39 +1,35 @@
-import { Avatar, lighten, Tooltip } from '@material-ui/core';
+import { Avatar, AvatarGroup, lighten } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import PersonIcon from '@material-ui/icons/Person';
 import { keys, max, values } from 'lodash';
 import * as React from 'react';
+import { useMemo } from 'react';
 
 export interface EstimationResultBarProps {
   estimates: Array<{ value: string; user: { _id: string } }>;
 }
 
 export function EstimationResultBar({ estimates }: EstimationResultBarProps) {
-  const results = estimates.reduce<Record<string, string[]>>(function (acc, currentValue) {
-    (acc[currentValue.value] = acc[currentValue.value] || []).push(currentValue.user._id);
-    return acc;
-  }, {});
+  const results = useMemo(() => {
+    if (estimates) {
+      return estimates.reduce<Record<string, string[]>>(function (acc, currentValue) {
+        (acc[currentValue.value] = acc[currentValue.value] || []).push(currentValue.user._id);
+        return acc;
+      }, {});
+    }
+  }, [estimates]);
 
   const maxWeight = max(values(results).map(v => v.length)) ?? 0;
 
   return (
-    <Box sx={{ width: '100%' }}>
-      {(!results || keys(results).length === 0) && (
-        <Box
-          sx={{
-            width: '100%',
-            bgcolor: '#BBB'
-          }}
-        >
-          &nbsp;
-        </Box>
-      )}
+    <>
       {results &&
         keys(results).map((value, index) => {
           const users = results[value];
           const weight = users.length;
+
           return (
-            <div key={index}>
+            <Box key={index} sx={{ flexGrow: weight }}>
               <Box
                 sx={{
                   flexGrow: weight,
@@ -45,20 +41,18 @@ export function EstimationResultBar({ estimates }: EstimationResultBarProps) {
                 {value}
               </Box>
               <Box sx={{ p: 2, display: 'flex', justifyContent: 'center' }}>
-                {users.map(userId => (
-                  <Box key={userId} sx={{ display: 'flex', alignItems: 'center' }}>
-                    <Tooltip title={userId}>
-                      <Avatar variant="rounded" sx={{ mx: 1, my: 1 }}>
-                        <PersonIcon />
-                      </Avatar>
-                    </Tooltip>
-                  </Box>
-                ))}
+                <AvatarGroup max={6}>
+                  {users.map(userId => (
+                    <Avatar key={userId}>
+                      <PersonIcon />
+                    </Avatar>
+                  ))}
+                </AvatarGroup>
               </Box>
-            </div>
+            </Box>
           );
         })}
-    </Box>
+    </>
   );
 }
 
@@ -74,17 +68,13 @@ export function EstimationResultBarHidden({ estimates }: EstimationResultBarProp
         &nbsp;
       </Box>
       <Box sx={{ p: 2, display: 'flex', justifyContent: 'center' }}>
-        {estimates
-          .map(e => e.user)
-          .map(user => (
-            <Box key={user._id} sx={{ display: 'flex', alignItems: 'center' }}>
-              <Tooltip title={user._id}>
-                <Avatar variant="rounded" sx={{ mx: 1, my: 1 }}>
-                  <PersonIcon />
-                </Avatar>
-              </Tooltip>
-            </Box>
+        <AvatarGroup max={6}>
+          {estimates.map(e => (
+            <Avatar key={e.user._id}>
+              <PersonIcon />
+            </Avatar>
           ))}
+        </AvatarGroup>
       </Box>
     </Box>
   );
--- src/components/estimate/EstimationToolbar.tsx
+++ src/components/estimate/EstimationToolbar.tsx
@@ -1,13 +1,14 @@
-import { Avatar, Box } from '@material-ui/core';
+import { Avatar, AvatarGroup, Box, Divider, InputBase, Paper } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import PersonIcon from '@material-ui/icons/Person';
 import ReplayIcon from '@material-ui/icons/Replay';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { IssueState, useActiveIssueQuery, useResetActiveIssueMutation, useUpdateActiveIssueMutation } from '../../generated/graphql';
 import { usePusherChannel } from '../AppContext';
 
@@ -18,34 +19,55 @@ export default function EstimationToolbar() {
   const [resetActiveIssue] = useResetActiveIssueMutation();
   const [updateActiveIssue] = useUpdateActiveIssueMutation();
 
+  const [issueName, setIssueName] = useState(data?.getActiveIssue?.name);
+  useEffect(() => {
+    setIssueName(data?.getActiveIssue?.name);
+  }, [data]);
+
   return (
-    <AppBar position="static" color="transparent" elevation={0}>
+    <AppBar position="static">
       <Toolbar>
-        <IconButton edge="start" title="Reset Issue" onClick={() => resetActiveIssue()}>
-          <ReplayIcon />
-        </IconButton>
-        <IconButton
-          title="Toggle Issue State"
-          onClick={() =>
-            updateActiveIssue({ variables: { state: data?.getActiveIssue?.state === IssueState.COLLECT ? IssueState.DISCUSS : IssueState.COLLECT } })
-          }
-        >
-          {data?.getActiveIssue?.state === IssueState.COLLECT ? <VisibilityIcon /> : <VisibilityOffIcon />}
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Estimate: {data?.getActiveIssue?.name}
-        </Typography>
-        <Box
-          sx={{
-            display: 'flex'
-          }}
-        >
-          {channel &&
-            Object.keys(channel?.members?.members).map(memberId => (
-              <Avatar variant="rounded" key={memberId}>
-                <PersonIcon />
-              </Avatar>
-            ))}
+        <Paper sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}>
+          <IconButton sx={{ p: '10px' }} aria-label="menu">
+            <MoreVertIcon />
+          </IconButton>
+          <InputBase
+            autoComplete="off"
+            onChange={e => {
+              setIssueName(e.target.value);
+            }}
+            onBlur={e => updateActiveIssue({ variables: { name: e.target.value } })}
+            onKeyDown={e => {
+              if (e.key === 'Escape') setIssueName(data?.getActiveIssue?.name);
+              if (e.key === 'Enter') updateActiveIssue({ variables: { name: e.target.value } });
+            }}
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Rename Issue"
+            value={issueName}
+          />
+          <IconButton
+            type="submit"
+            sx={{ p: '10px' }}
+            onClick={() =>
+              updateActiveIssue({ variables: { state: data?.getActiveIssue?.state === IssueState.COLLECT ? IssueState.DISCUSS : IssueState.COLLECT } })
+            }
+          >
+            {data?.getActiveIssue?.state === IssueState.COLLECT ? <VisibilityIcon /> : <VisibilityOffIcon />}
+          </IconButton>
+          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+          <IconButton color="primary" sx={{ p: '10px' }} onClick={() => resetActiveIssue()}>
+            <ReplayIcon />
+          </IconButton>
+        </Paper>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', flexGrow: 1 }}>
+          <AvatarGroup max={6}>
+            {channel &&
+              Object.keys(channel?.members?.members).map(memberId => (
+                <Avatar key={memberId}>
+                  <PersonIcon />
+                </Avatar>
+              ))}
+          </AvatarGroup>
         </Box>
       </Toolbar>
     </AppBar>
