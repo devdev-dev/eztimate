a907a9b1222f22b2aa79faf71582d0b8bd843c5d
Hendrik Schuster <h.schuster@deviantdev.com>
2021-10-02 09:37:13 +0200

    Remove duplicated code for the OnlineBadgeAvatar and simplify the badge
    - no animation for the badge anymore :(

--- src/components/estimate/EstimationToolbarUserPanel.tsx
+++ src/components/estimate/EstimationToolbarUserPanel.tsx
@@ -1,14 +1,12 @@
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import CircleIcon from '@mui/icons-material/Circle';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import { AvatarGroup, Badge, Box, IconButton } from '@mui/material';
+import { Avatar, AvatarGroup, Badge, Box, IconButton } from '@mui/material';
 import Stack from '@mui/material/Stack';
-import { styled } from '@mui/system';
 import * as React from 'react';
 import { useActiveIssueQuery, useGetUserQuery } from '../../generated/graphql';
 import { useIssueId, usePusherChannel } from '../AppContext';
 import UserAvatar, { UserAvatarSkeleton } from '../UserAvatar';
-import OnlineBadgeAvatar from './OnlineBadgeAvatar';
 
 export default function EstimationToolbarUserPanel() {
   const channel = usePusherChannel();
@@ -31,9 +29,9 @@ export default function EstimationToolbarUserPanel() {
           })}
       </AvatarGroup>
       <IconButton onClick={() => handleCopyID(issueId)}>
-        <OnlineBadgeAvatar>
+        <Avatar>
           <PersonAddIcon />
-        </OnlineBadgeAvatar>
+        </Avatar>
       </IconButton>
     </Box>
   );
@@ -41,49 +39,23 @@ export default function EstimationToolbarUserPanel() {
 
 function UserAvatarWrapper({ userId, hasEstimated }: { userId: string; hasEstimated?: boolean }) {
   const { data, loading, error } = useGetUserQuery({ variables: { id: userId } });
-  const BadgeIcon = <CircleIcon sx={{ height: '16px', width: '16px', zIndex: 500 }} />;
-  const CheckBadgeIcon = <CheckCircleIcon sx={{ height: '16px', width: '16px', zIndex: 500 }} />;
-
+  const BadgeIcon = hasEstimated ? (
+    <CheckCircleIcon sx={{ height: '16px', width: '16px', zIndex: 500 }} />
+  ) : (
+    <CircleIcon sx={{ height: '16px', width: '16px', zIndex: 500 }} />
+  );
   return (
     <>
       {data && !loading && (
         <Stack direction="row" spacing={2}>
-          <OnlineBadge overlap="circular" anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }} badgeContent={hasEstimated ? CheckBadgeIcon : BadgeIcon}>
+          <Badge sx={{ color: '#44b700' }} overlap="circular" anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }} badgeContent={BadgeIcon}>
             {data?.getUser ? <UserAvatar user={data?.getUser} /> : <UserAvatarSkeleton />}
-          </OnlineBadge>
+          </Badge>
         </Stack>
       )}
     </>
   );
 }
-
-const OnlineBadge = styled(Badge)(() => ({
-  '& .MuiBadge-badge': {
-    zIndex: 400,
-    color: '#44b700',
-    width: 0,
-    '&::after': {
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      borderRadius: '50%',
-      animation: 'ripple 2s 1 ease',
-      border: '1px solid transparent',
-      backgroundClip: 'padding-box',
-      content: '""',
-      transform: 'scale(0.6)'
-    }
-  },
-  '@keyframes ripple': {
-    '0%': {
-      opacity: 0.8
-    },
-    '100%': {
-      transform: 'scale(2.5)',
-      opacity: 0
-    }
-  }
-}));
 
 function handleCopyID(issueId: string) {
   const origin = typeof window !== 'undefined' && window.location.origin ? window.location.origin : '';
