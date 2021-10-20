dccc19feca26c95e26f43364397700a96f8cff05
Hendrik Schuster <h.schuster@deviantdev.com>
2021-10-20 23:31:40 +0200

    Fix another build break because of types

--- src/pages/api/s3.ts
+++ src/pages/api/s3.ts
@@ -1,7 +1,9 @@
 import aws from 'aws-sdk';
+import { ManagedUpload } from 'aws-sdk/lib/s3/managed_upload';
 import { IncomingForm } from 'formidable';
 import fs from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
+import SendData = ManagedUpload.SendData;
 
 export const config = {
   api: {
@@ -37,7 +39,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       Body: reqForm.data,
       ACL: 'public-read'
     },
-    function (s3Err, data) {
+    function (s3Err: Error, data: SendData) {
       if (s3Err) throw s3Err;
 
       if (reqForm.oldiamge) {
