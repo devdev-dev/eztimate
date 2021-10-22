2fd159b6920c24d6c7e5a1b9c06bb8145c927c0b
Hendrik Schuster <h.schuster@deviantdev.com>
2021-10-23 00:05:35 +0200

    Add a warning to say things about the instant estimation thingy

--- src/components/estimate/settings/EstimationInformation.tsx
+++ src/components/estimate/settings/EstimationInformation.tsx
@@ -1,4 +1,4 @@
-import { Box, TextField } from '@mui/material';
+import { Alert, Box, TextField } from '@mui/material';
 import React, { useRef, useState } from 'react';
 import { Estimate, User, useUpdateActiveUserMutation } from '../../../generated/graphql';
 
@@ -9,6 +9,7 @@ export interface EstimationInformationProps {
 export default function EstimationInformation({ estimate }: EstimationInformationProps) {
   return (
     <Box sx={{ p: 1, display: 'flex', gap: 2, flexDirection: 'column' }}>
+      <Alert severity="warning">This is an instant estimation session which is public. All settings are temporary and can be changed by anyone.</Alert>
       <TextField label="Your id" variant="outlined" value={estimate._id} fullWidth disabled />
     </Box>
   );
