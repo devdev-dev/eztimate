bac1340334409f98a7794341935107b86976d9ee
Hendrik Schuster <h.schuster@deviantdev.com>
2021-10-26 22:00:44 +0200

    Introduce loading skeletons for the instant estimate content

--- src/components/estimate/body/EstimationResultBar.tsx
+++ src/components/estimate/body/EstimationResultBar.tsx
@@ -1,4 +1,5 @@
-import { Avatar, AvatarGroup, Box, BoxProps, lighten, styled } from '@mui/material';
+import { Avatar, AvatarGroup, Box, BoxProps, lighten, Skeleton, styled } from '@mui/material';
+import { bgcolor } from '@mui/system';
 import { keys, max, values } from 'lodash';
 import * as React from 'react';
 import { useMemo } from 'react';
@@ -46,6 +47,21 @@ export function EstimationResultBar({ estimates, hideResults }: EstimationResult
         );
       })}
     </>
+  );
+}
+
+export function EstimationResultBarSkeleton() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <StyledResultBar sx={{ bgcolor: 'transparent' }} weight={1} maxWeight={1} disabled>
+        <Skeleton variant="rectangular" width="100%" height="24px" />
+      </StyledResultBar>
+      <AvatarGroup max={10} sx={{ p: 2, justifyContent: 'center' }}>
+        <Skeleton variant="circular">
+          <Avatar />
+        </Skeleton>
+      </AvatarGroup>
+    </Box>
   );
 }
 
