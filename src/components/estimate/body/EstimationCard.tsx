cd38085057052de3ed768cd29e7a8d556791d31f
Hendrik Schuster <h.schuster@deviantdev.com>
2021-10-27 20:49:10 +0200

    Extract the EstimationCard component from the EstimationCardStack

---
+++ src/components/estimate/body/EstimationCard.tsx
@@ -0,0 +1,60 @@
+import { css } from '@emotion/css';
+import { Theme, useTheme } from '@mui/material';
+import * as React from 'react';
+import classNames from 'classnames';
+
+interface EstimationCardProps {
+  value: string;
+  selected: boolean;
+  onClick: (value: string | null) => void;
+}
+
+const EstimationCard = ({ value, selected, onClick }: EstimationCardProps) => {
+  const theme = useTheme();
+  return (
+    <div className={classNames(styles.card(theme), { selected: selected })} onClick={() => onClick(selected ? null : value)}>
+      <h3>{value}</h3>
+    </div>
+  );
+};
+
+const styles = {
+  card: (theme: Theme) => css`
+    width: 150px;
+    height: 100px;
+    border-radius: 5px;
+    color: #fff;
+    transform: rotateX(30deg) rotateY(-15deg) rotate(45deg);
+    transition: transform 0.5s ease, background-position 0.5s linear;
+    overflow: hidden;
+    cursor: pointer;
+    background: linear-gradient(135deg, ${theme.palette.primary.light} 0%, ${theme.palette.primary.main} 25%, ${theme.palette.secondary.main} 55%);
+    background-size: 400% 400%;
+    box-shadow: 20px 20px 60px rgba(34, 50, 84, 0.5), 1px 1px 0 1px ${theme.palette.primary.dark};
+    animation: CardSelect 0.5s linear;
+
+    &:hover {
+      transform: rotateX(30deg) rotateY(-15deg) rotate(45deg) translate(20px, 0px);
+    }
+
+    &.selected {
+      transform: rotateX(30deg) rotateY(-15deg) rotate(45deg) translate(20px, 0px);
+      background-position: 50% 50%;
+    }
+
+    &.skeleton {
+      background: white;
+      box-shadow: none;
+    }
+
+    > h3 {
+      position: absolute;
+      top: 0;
+      right: 10px;
+      font-size: 24px;
+      font-weight: 600;
+    }
+  `
+};
+
+export default EstimationCard;
--- src/components/estimate/body/EstimationCardStack.tsx
+++ src/components/estimate/body/EstimationCardStack.tsx
@@ -3,16 +3,17 @@ import { Skeleton, Theme, useTheme } from '@mui/material';
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { useActiveIssueQuery, useActiveUserQuery, useUpdateUserEstimateMutation } from '../../../generated/graphql';
+import EstimationCard from './EstimationCard';
 
 const EstimationCardStack = () => {
   const theme = useTheme();
-  const [userEstimate, setUserEstimate] = useState<string | undefined>(undefined);
+  const [userEstimate, setUserEstimate] = useState<string | null>(null);
 
   const { data, loading } = useActiveIssueQuery();
   const { data: userData } = useActiveUserQuery();
 
   useEffect(() => {
-    setUserEstimate(data?.getActiveIssue?.estimates.find(e => e.user._id === userData?.getActiveUser?._id)?.value);
+    setUserEstimate(data?.getActiveIssue?.estimates.find(e => e.user._id === userData?.getActiveUser?._id)?.value ?? null);
   }, [data, userData, setUserEstimate]);
 
   const [updateEstimate] = useUpdateUserEstimateMutation();
@@ -37,23 +38,15 @@ const EstimationCardStack = () => {
           {data.getActiveIssue?.stack.map((value, index) => {
             const isEstimatedCardValue = userEstimate === value;
             return (
-              <div
+              <EstimationCard
                 key={index}
-                className={isEstimatedCardValue ? 'selected' : ''}
-                onClick={() => {
-                  if (userEstimate && isEstimatedCardValue) {
-                    updateEstimate({ variables: { value: null } });
-                    setUserEstimate(undefined);
-                  } else {
-                    updateEstimate({
-                      variables: { value }
-                    });
-                    setUserEstimate(value);
-                  }
+                value={value}
+                selected={isEstimatedCardValue}
+                onClick={value => {
+                  updateEstimate({ variables: { value: value } });
+                  setUserEstimate(value);
                 }}
-              >
-                <h3>{value}</h3>
-              </div>
+              />
             );
           })}
         </div>
@@ -71,44 +64,6 @@ const styles = {
     margin: 50px 75px 50px ${theme.spacing(0)};
     user-select: none;
     transform-style: preserve-3d;
-
-    & > div {
-      width: 150px;
-      height: 100px;
-      border-radius: 5px;
-      color: #fff;
-      transform: rotateX(30deg) rotateY(-15deg) rotate(45deg);
-      transition: transform 0.5s ease, background-position 0.5s linear;
-      overflow: hidden;
-      cursor: pointer;
-      background: linear-gradient(135deg, ${theme.palette.primary.light} 0%, ${theme.palette.primary.main} 25%, ${theme.palette.secondary.main} 55%);
-      background-size: 400% 400%;
-      box-shadow: 20px 20px 60px rgba(34, 50, 84, 0.5), 1px 1px 0 1px ${theme.palette.primary.dark};
-      animation: CardSelect 0.5s linear;
-
-      &:hover {
-        transform: rotateX(30deg) rotateY(-15deg) rotate(45deg) translate(20px, 0px);
-        background-position: 50% 50%;
-      }
-
-      &.selected {
-        transform: rotateX(30deg) rotateY(-15deg) rotate(45deg) translate(20px, 0px);
-        background-position: 50% 50%;
-      }
-
-      &.skeleton {
-        background: white;
-        box-shadow: none;
-      }
-
-      > h3 {
-        position: absolute;
-        top: 0;
-        right: 10px;
-        font-size: 24px;
-        font-weight: 600;
-      }
-    }
   `
 };
 
