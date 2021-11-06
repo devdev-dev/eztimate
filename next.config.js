57f904015dc55ada923856e5d01e679eb7c9c225
Hendrik Schuster <h.schuster@deviantdev.com>
2021-11-06 22:19:14 +0100

    Upgrade to nextjs12

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-  webpack: (config, options) => {
-    config.experiments = { topLevelAwait: true };
-    // Define an environment variable so source code can check whether or not it's running on the server
-    config.plugins.push(
-      new options.webpack.DefinePlugin({
-        'process.env.NEXT_IS_SERVER': JSON.stringify(options.isServer.toString())
-      })
-    );
-
+  webpack: config => {
     config.module.rules.push({
       test: /\.(graphql|gql)$/,
       exclude: /node_modules/,
